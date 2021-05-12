99fc193da2ed3ac6f55430988fb5bb076979d776
Gian Eric <gianeric08@gmail.com>
2021-05-12 01:05:48 -0300

    [App] Add indice

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const pokedex = require('./templates/pokedex.json');
+const index   = require('./templates/index.json');
 const app     = express();
 const version = 'v1'
 
 app.use(express.json());
+
+//Listar Indice
+app.get('/pokedex/'+version, (req, res) => {
+    res.send(index);
+});
 
 //Listar Pokedex
 app.get('/pokedex/'+version+'/all', (req, res) => {
@@ -43,5 +49,13 @@ app.get('/pokedex/'+version+'/type/:type', (req, res) => {
     res.send(pokedexItens);
 });
 
+//Listar Pokedex por ataque
+app.get('/pokedex/'+version+'/attack', (req, res) => {   
+});
+
+//Listar Pokedex por defesa 
+app.get('/pokedex/'+version+'/defense', (req, res) => {   
+
+});
 
 app.listen(3000, () => console.log('Listening on port 3000'));
